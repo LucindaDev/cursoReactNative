88d0a5da852bc8970a963a0bf18b4d601dffd6d2
lucindadev <lucindadsanchez@gmail.com>
2025-01-23 10:22:52 -0700

    Add ESLint configuration and integrate Prettier; update App.js with new layout and styles

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable} from 'react-native';
+import cat from './assets/cat.jpg'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Image source={cat} style={{ 
+        width: 200, 
+        height: 220
+        }} />
+      <Text style={{color:'#fff'}}>LUCINDAPP</Text>
+      <Pressable style={{backgroundColor:'red', width: 150, height:150, borderRadius: 100, alignItems: 'center', justifyContent: 'center'}} onPress={() => alert('Hello World!')}>
+        <Text>Presiona aquí</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#181C14',
     alignItems: 'center',
     justifyContent: 'center',
   },
